08a7534962c94ca2e68910abc17d651a1f8deb22
David Wolff <david@dav.dev>
2023-05-08 23:45:17 +0200

    Implement 'AT+SEND' in hoppy-tester

--- hoppy-tester/src/main.rs
+++ hoppy-tester/src/main.rs
@@ -1,7 +1,9 @@
-use std::io;
+use std::{io, thread};
 use std::io::{Read, Write};
 use std::time::Duration;
 use crate::command_parser::{Commands, CommandsError};
+use CommandsError::IoError;
+use io::ErrorKind::TimedOut;
 
 mod command_parser;
 
@@ -33,9 +35,6 @@ fn main() {
 		.expect("couldn't clone serial port");
 	
 	for command_result in Commands::in_stream(reader) {
-		use CommandsError::IoError;
-		use io::ErrorKind::TimedOut;
-		
 		let command = match command_result {
 			Ok(command) => command,
 			Err(IoError(TimedOut)) => continue,
@@ -53,9 +52,52 @@ fn main() {
 fn handle_command(mut port: impl Read + Write, command: Vec<u8>) -> Result<(), io::Error> {
 	if command == b"AT" {
 		port.write(b"AT,OK\r\n")?;
+	} else if command.starts_with(b"AT+SEND=") {
+		handle_send(port, &command[8..])?;
 	} else {
 		port.write(b"AT,ERR:CMD\r\n")?;
 	}
+	
+	Ok(())
+}
+
+fn handle_send(mut port: impl Read + Write, args: &[u8]) -> Result<(), io::Error> {
+	let Ok(bytes_to_receive) = String::from_utf8_lossy(args).parse::<usize>() else {
+		port.write(b"AT,ERR:PARA\r\n")?;
+		return Ok(());
+	};
+	
+	if !(1..250).contains(&bytes_to_receive) {
+		port.write(b"AT,ERR:PARA\r\n")?;
+		return Ok(());
+	}
+	
+	port.write(b"AT,OK\r\n")?;
+	
+	let mut buffer: Vec<u8> = vec![0; bytes_to_receive];
+	let mut available_buffer = buffer.as_mut_slice();
+	
+	loop {
+		let length = match port.read(available_buffer) {
+			Ok(length) => length,
+			Err(err) => match err.kind() {
+				TimedOut => continue,
+				_ => return Err(err),
+			},
+		};
+		
+		available_buffer = &mut available_buffer[length..];
+		
+		if available_buffer.is_empty() {
+			break;
+		}
+	}
+	
+	println!("Received data: {:?}", String::from_utf8_lossy(&buffer));
+	
+	port.write(b"AT,SENDING\r\n")?;
+	thread::sleep(Duration::from_secs(1));
+	port.write(b"AT,SENDED\r\n")?;
 	
 	Ok(())
 }
